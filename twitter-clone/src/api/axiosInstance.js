9d4189c2dbd3314d7839698b9ae9ead7d29ef45c
seooyuun <seoyuni2002@gmail.com>
2025-05-31 10:57:18 +0900

    fix: axiosInstance 수정

--- twitter-clone/src/api/axiosInstance.js
+++ twitter-clone/src/api/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.efub-tweeter.p-e.kr/",
+  baseURL: "https://api.efub-tweeter.p-e.kr",
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 export default instance;
