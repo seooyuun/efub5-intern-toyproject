4fdea9fc445862d05acab5c88e61d8b91cde3746
seooyuun <seoyuni2002@gmail.com>
2025-06-04 08:23:01 +0900

    fix: 도메인 변경

--- twitter-clone/src/api/axiosInstance.js
+++ twitter-clone/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.efub-tweeter.p-e.kr",
+  baseURL: "https://api.efub5-tweeter.p-e.kr/",
   headers: {
     "Content-Type": "application/json",
   },
